bd6752deddc18082ee86cac1c4b3dffb41170d3b
Albina <darkcrashma@mail.ru>
2019-01-09 14:47:41 +0800

    #add: Добавлен аккордеон в футер

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 $(document).ready( function() {
+    accordeonFooter();
     $('.header-slider').slick({
         arrows: true,
         dots: false,
         slidesToShow: 1,
         slidesToScroll: 1,
     });
+
 });
+
+function accordeonFooter () {
+    var $list = $('.footer-widgets__pages-title');
+    var $cont = $('.footer-widgets__pages-content');
+    $list.on('click', function (e) {
+        e.preventDefault();
+        var $this = $(this).next(".footer-widgets__pages-content");
+        // if (!$this) {
+        //     $cont.slideUp();
+        // }
+        if(!$.contains($list.get(0), e.target)) {
+            $cont.slideUp();
+        }
+            $this.slideToggle();
+    });
+}
+
